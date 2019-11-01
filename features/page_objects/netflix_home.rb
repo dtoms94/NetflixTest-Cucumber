11e897d463e0c7ec60ef0862ead9b97354ba608e
Dylan <dtoms94@gmail.com>
2019-11-01 18:24:44 -0400

    Used XPath to navigate to the Netflix Login In page and verify the page

--- features/page_objects/netflix_home.rb
+++ features/page_objects/netflix_home.rb
@@ -4,11 +4,18 @@ class NetflixHome
   include PageObject
 
   HOME_URL = 'https://www.netflix.com/'
+  LOGIN_URL = 'https://www.netflix.com/login'
 
   g(:netflix_logo, id: 'netflix-logo')
+  link(:sign_in_button, xpath: "//div[@class='our-story-header']/a")
+  form(:login_form, xpath: "//form[@class='login-form']")
 
   def netflix_url
     HOME_URL
+  end
+
+  def login_url
+    LOGIN_URL
   end
 
 end
